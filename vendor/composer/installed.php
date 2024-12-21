72d32884796ed15a161cae19bf21c6e86e4a1858
yakoffka <yagithub@mail.ru>
2024-12-21 13:23:47 +0300

    создание структуры проекта
    - добавление composer.json;
    - добавление gitignore;

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yakoffka/dijkstras_algorithm',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dde35e25ca8e4a4ed1d201dfae4c22deee822855',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yakoffka/dijkstras_algorithm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dde35e25ca8e4a4ed1d201dfae4c22deee822855',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
