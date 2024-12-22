3f88a0882454d1bc258aa61a45cc45ed98bc5170
yakoffka <yagithub@mail.ru>
2024-12-22 16:28:49 +0300

    feature: добавление unit-тестирования

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Yakoffka\DijkstrasAlgorithm\Tests;
+
+/**
+ * Пример вызова тестов: vendor/bin/phpunit
+ */
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Yakoffka\DijkstrasAlgorithm\Tests\Unit;
+
+use Yakoffka\DijkstrasAlgorithm\Tests\TestCase;
+
+/**
+ *
+ */
+class ExampleTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testAddition(): void
+    {
+        $a = true;
+
+        static::assertTrue($a);
+    }
+}
